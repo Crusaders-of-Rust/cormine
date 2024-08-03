36f2948979026443fc007387f94080b95c8c1bdc
clubby789 <jamie@hill-daniel.co.uk>
2024-08-03 21:04:41 +0100

    Rotate sun around level

--- src/sky.rs
+++ src/sky.rs
@@ -29,10 +29,16 @@ pub fn add_sun(
 }
 
 pub fn update_sun_position(
+    time: Res<Time>,
     mut sun: Query<&mut Transform, (With<Sun>, Without<Camera>)>,
     player: Query<&Transform, With<Camera>>,
 ) {
     let mut sun = sun.single_mut();
     let player = player.single();
-    sun.translation = player.translation + Vec3::new(0.0, 50.0, 1000.0);
+    let mut sun_dir = Transform::from_translation(Vec3::new(0.0, 1.0, 1.0));
+    // Rotate around the 'planet' every minute
+    sun_dir.rotate_x(f32::to_radians(time.elapsed_seconds() * 6.0));
+    sun.translation = player.translation + sun_dir.forward().as_vec3() * 1000.0;
+    let up = sun.up();
+    sun.look_at(player.translation, up);
 }
